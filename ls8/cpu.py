c9c3989e82b5207488041dde1c195d39512cfacf
Michael Stoffer <mstoffer@michaelstoffer.com>
2020-08-06 08:08:09 -0400

    Day 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,16 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.HLT = 0b00000001
+
+    def ram_read(self, pc):
+        return self.ram[pc]
+
+    def ram_write(self, pc, word):
+        self.ram[pc]= word
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +71,19 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        while running:
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if IR ==  0b10000010:  # LDI instruction
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif IR == 0b01000111:  # PRN instruction
+                print(self.reg[operand_a])
+                self.pc += 2
+            elif IR == self.HLT:
+                running = False
+            else:
+                print(f"Unknown instruction {IR}")
